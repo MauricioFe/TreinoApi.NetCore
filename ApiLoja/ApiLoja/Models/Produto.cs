22303ddf3eb3a4408200a3f116a0df4e7b21f9c9
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-13 09:49:03 -0300

    Criação dos models e suas props

---
+++ ApiLoja/ApiLoja/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiLoja.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Categoria_id { get; set; }
+    }
+}
