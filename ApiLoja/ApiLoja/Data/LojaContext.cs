902d53299e67ed96179e262034fb2d5b340f9116
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-13 10:18:41 -0300

    Criação do contexto para o banco de dados

---
+++ ApiLoja/ApiLoja/Data/LojaContext.cs
@@ -0,0 +1,18 @@
+﻿using ApiLoja.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiLoja.Data
+{
+    public class LojaContext : DbContext
+    {
+        public LojaContext(DbContextOptions<LojaContext> options) : base(options) { }
+
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
