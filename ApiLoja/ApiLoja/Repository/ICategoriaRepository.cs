fbacdfd377b1e201803d94e5e2506c130007b0ae
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-13 18:14:23 -0300

    Criação da interface categoria repositorio

---
+++ ApiLoja/ApiLoja/Repository/ICategoriaRepository.cs
@@ -0,0 +1,17 @@
+﻿using ApiLoja.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiLoja.Repository
+{
+    interface ICategoriaRepository
+    {
+        IEnumerable<Categoria> GetAll();
+        Categoria Find(int id);
+        void Add(Categoria categoria);
+        void Update(int id, Categoria categoria);
+        void Remove(int id);
+    }
+}
